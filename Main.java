223d4c8dd4f1581c1a5850d4c795f9ddb97c7b2e
Nathaniel  Earl Navarra <earlbel25@gmail.com>
2026-03-05 21:25:41 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,4 +9,15 @@ abstract class Hero {
 
     
     public abstract void attack();
+
+}
+class Warrior extends Hero {
+    public Warrior(String name) {
+        super(name, 120); 
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(name + " swings a heavy Iron Sword!");
+    }
 }
